ef7649267f7bb0330a7e9e77d6368d07d42abfed
Dharizza Espinach B. <dharizza@gmail.com>
2024-12-19 20:49:44 +0000

    Use Url and Link classes to create links.

--- project/web/modules/custom/hello_world/src/Controller/HelloController.php
+++ project/web/modules/custom/hello_world/src/Controller/HelloController.php
@@ -6,6 +6,8 @@ namespace Drupal\hello_world\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\node\Entity\Node;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
 
 /**
  * Returns responses for Hello World routes.
@@ -34,10 +36,13 @@ final class HelloController extends ControllerBase {
 
     if ($person !== NULL) {
       if ($node) {
+        $url = Url::fromRoute('entity.node.canonical', ['node' => $nid]);
+        $link = Link::fromTextAndUrl('here', $url);
+
         $output = $this->t('Hello @person_name! The node with ID @nid is @title', [
           '@person_name' => $person,
           '@nid' => $nid,
-          '@title' => $node->toLink()->toString(),
+          '@title' => $link->toString(),
         ]);
       } else {
         $output = $this->t('Hello @person_name!', [
