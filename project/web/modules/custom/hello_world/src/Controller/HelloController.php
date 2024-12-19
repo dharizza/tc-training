139e9fa7988fda9fe866f06d58bf1c314b227fa2
Dharizza Espinach B. <dharizza@gmail.com>
2024-12-19 19:32:26 +0000

    Added hello world module with basic controller and route.

---
+++ project/web/modules/custom/hello_world/src/Controller/HelloController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\hello_world\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Returns responses for Hello World routes.
+ */
+final class HelloController extends ControllerBase {
+
+  /**
+   * Builds the response.
+   */
+  public function hello(): array {
+
+    $build['content'] = [
+      '#type' => 'item',
+      '#markup' => $this->t('It works!'),
+    ];
+
+    return $build;
+  }
+
+}
