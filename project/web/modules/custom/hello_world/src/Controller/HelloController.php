d9d3d6792c8224a50638c74f78b184959ff48ade
Dharizza Espinach B. <dharizza@gmail.com>
2024-12-19 20:37:05 +0000

    Add new dynamic route that interacts with nodes.

--- project/web/modules/custom/hello_world/src/Controller/HelloController.php
+++ project/web/modules/custom/hello_world/src/Controller/HelloController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Drupal\hello_world\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
 
 /**
  * Returns responses for Hello World routes.
@@ -14,11 +15,35 @@ final class HelloController extends ControllerBase {
   /**
    * Builds the response.
    */
-  public function hello($person = NULL): array {
+  public function hello($person = NULL, $nid = NULL): array {
     $output = $this->t('Hello World!');
 
+    if ($nid !== NULL) {
+      $node = Node::load($nid);
+      
+      // Alternatives to printing values using functions and properties.
+      // ksm($node->getTitle());
+      // ksm($node->title->value);
+
+      // ksm($node->get('body')->getValue());
+      // ksm($node->body->value);
+
+      // ksm($node->get('field_specialty'));
+      // ksm($node->field_specialty->entity->name->value);
+    }
+
     if ($person !== NULL) {
-      $output = $this->t('Hello @person_name!', ['@person_name' => $person]);
+      if ($node) {
+        $output = $this->t('Hello @person_name! The node with ID @nid is @title', [
+          '@person_name' => $person,
+          '@nid' => $nid,
+          '@title' => $node->getTitle(),
+        ]);
+      } else {
+        $output = $this->t('Hello @person_name!', [
+          '@person_name' => $person,
+        ]);
+      }
     }
 
     $build['content'] = [
